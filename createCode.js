b6062efdc375e813d64d0e6af27b918e230df7b8
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-03-28 13:04:51 +0530

    added the array creating method

---
+++ createCode.js
@@ -0,0 +1,14 @@
+// Array :- It is collection more the single value inside an variable
+
+// Array define by literal method
+const Arr = [12,"Mayur" , 5.67];
+console.log(Arr);
+
+
+// example 1
+
+const Arr2 =[1,"Gaurav",98,98.98];
+for(let i=0;i<Arr2.length;i++){
+    console.log(Arr2[i]);
+    // console.log("\n");
+}
