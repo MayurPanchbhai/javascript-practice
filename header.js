d45436d2795e816793f471a060fb3c8fc75b01aa
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-03-28 15:38:55 +0530

    added the common header and also the change some design

---
+++ header.js
@@ -0,0 +1,56 @@
+let header = document.getElementById("header");
+
+header.innerHTML=`
+<nav class="navbar  d-fixed">
+        <div class="container-fluid px-5">
+            <div>
+                <a class="navbar-brand d-flex align-items-center " href="#">
+                    <img src="/javascript-practice/img/8e97ca232981539af46c42ce525daeb0.jpg" alt="Logo" class="d-inline-block align-text-top" style="width: 50px; height: 50px; border-radius: 50%; margin-right: 12px;">
+                    Mayur Panchbhai
+                </a>
+            </div>
+            <div class="d-flex justify-content-evenly middle-div " >
+                <a class="" href="../index.html">
+                  Home
+                </a>
+                <a class="" href="https://www.linkedin.com/in/panchbhaimayur-921131242/" target="_blank">
+                  <i class="fa-brands fa-linkedin-in"></i> Linked In
+                </a>
+                <a class="" href="https://twitter.com/Mayur_Panchbh_i" target="_blank">
+                  <i class="fa-brands fa-x-twitter"></i>
+                  Twitter
+                </a>
+            </div>
+            <div>
+                <a href="https://wa.me/qr/J3A6M4GKFHD5D1"><button type="button" class="btn btn-light" target="_blank">Whatapp <i class="fa-brands fa-whatsapp"></i> </button></a>
+            </div>
+        </div>
+      </nav>
+`
+
+// add this href for the  twitter
+{/* <a class="" href="#">
+                    <i class="fa-brands fa-linkedin-in"></i> Linked In
+                  </a> */}
+
+
+{/* <div class="text-gray-600 body-font">
+<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+<a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+   
+  <span class="ml-3 text-xl">Mayur Panchbhai</span>
+</a>
+<nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center" >
+  <a href="https://mayurpanchbhai.github.io/javascript-practice/" class="mr-5 hover:text-gray-900">Home</a>
+  <a class="mr-5 hover:text-gray-900" href="https://wa.me/qr/J3A6M4GKFHD5D1">Contact</a>
+  <a class="mr-5 hover:text-gray-900">Services</a>
+  <a class="mr-5 hover:text-gray-900">About</a>
+</nav>
+<button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" >
+  <a href="https://wa.me/qr/J3A6M4GKFHD5D1">Whatsapp</a>
+  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24" >
+    <path d="M5 12h14M12 5l7 7-7 7"></path>
+  </svg>
+</button>
+</div>
+</div> */}
