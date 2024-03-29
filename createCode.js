da7b31d63616225c7cdcccde9e846d49b7adb2a2
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-03-29 22:02:18 +0530

    added the copywithin() method

--- createCode.js
+++ createCode.js
@@ -1,13 +1,15 @@
 // Array :- It is collection more the single value inside an variable
-// concat :- It return a new array by merging two or more than two arrays.
+// copywithin -It copies the element from the own array and return to new array
 
-// e.g. let try to concat an array along with element directly if it is possible
-const lang1 = ["python","java"];
-const lang2 = ["script" , "tailwind"]
+// array.copyWithin(target, start, end)
+// target - position where the copied element take place
+// Start :- from which index it is gonna start the copying
+//  end : till the specified index
 
 
-const output =lang1.concat(lang2 ,"adding element","Github" ,8);
-
-console.log(output);//It only create new array and not change the original arrays
-console.log(lang1);//not change like me
-console.log(lang2);
+var arr=["AngularJS","Node.js","JQuery","Bootstrap"]  
+// place at 0th position, the element between 1st and 2nd position.  
+console.log(arr);
+var result=arr.copyWithin(0,1,2);  //if we given the target as 2/3 it will not replace the element before the indexcing 
+console.log(result); //And this methos change the original array also 
+console.log(arr); 
