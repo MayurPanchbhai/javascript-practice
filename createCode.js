2dffb03793f42ed1ceb3b11e1ed098b979f13701
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-03-30 23:22:59 +0530

    entries and every method

--- createCode.js
+++ createCode.js
@@ -1,15 +1,11 @@
 // Array :- It is collection more the single value inside an variable
-// copywithin -It copies the element from the own array and return to new array
+// every() : this method check the condition and return the boolean value if it is satisfy the condition 
+//array.every(callback(currentvalue,index,arr),thisArg)  
 
-// array.copyWithin(target, start, end)
-// target - position where the copied element take place
-// Start :- from which index it is gonna start the copying
-//  end : before the specified index
+const arr=[22,3,44,7,58,8]
 
-
-var arr=["AngularJS","Node.js","JQuery","Bootstrap" , 89]  
-// place from 0th position, the elements between 1st and 3rd position.  
-var result=arr.copyWithin(0,1,3);  //here the replacement not happen till the thired index it will end before it 
-//and also will keep repeating the second last index until it reaches the final array length
-console.log(result); 
-console.log(arr); 
+function test(element, index, array) {  
+    return index < 4;  
+  }  
+console.log([21,32,2,43].every(test)); //true  
+console.log([21,32,2,43,35].every(test)); //false  
