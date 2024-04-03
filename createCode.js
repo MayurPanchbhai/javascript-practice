369834b97dd43d267e54631192da6664c28cae06
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-04-03 21:46:09 +0530

    fill and flat method array

--- createCode.js
+++ createCode.js
@@ -1,11 +1,14 @@
 // Array :- It is collection more the single value inside an variable
-// every() : this method check the condition and return the boolean value if it is satisfy the condition 
-//array.every(callback(currentvalue,index,arr),thisArg)  
+// fill():- fills  elements of the given array with the specified static values.
 
-const arr=[22,3,44,7,58,8]
+// arr.fill(value,start, end) 
+// value - The static value to be filled.
+// start - It is optional. It represents the index from where the value starts filling. By default, it is 0.
+// end - It is optional. It represents the index where the value stops filling. By default, it is length-1.
 
-function test(element, index, array) {  
-    return index < 4;  
-  }  
-console.log([21,32,2,43].every(test)); //true  
-console.log([21,32,2,43,35].every(test)); //false  
+const arr1=[1,2,3,4,5];
+
+const arr1Copy = arr1.fill("m",1,4)//it will fill the "m" from start index to end index
+
+console.log(arr1);// and this method modify the original array
+console.log(arr1Copy);
