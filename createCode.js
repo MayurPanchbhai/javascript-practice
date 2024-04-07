fd0f42db0f442f65d125cbc129477f404f86dbd7
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-04-07 19:46:43 +0530

    find()

--- createCode.js
+++ createCode.js
@@ -1,18 +1,15 @@
 // Array :- It is collection more the single value inside an variable
-// filter(): extract the element from array which is satisfy the condition
+// find():-This method return the first element that satisfy coondition
 
-// array.filter(callback(currentvalue,index,arr),thisArg)  
+// Array.find(callback(,valueindex,arr,),ThisArg)
 
-// callback - It represents the function that test the condition.
-// currentvalue - The current element of array.
-// index - It is optional. The index of current element.
-// arr - It is optional. The array on which filter() operated.
-// thisArg - It is optional. The value to use as this while executing callback.
 
-const arr1=[1,2,3,4,5,6,7,8]
+const arr1=[1,2,3,4,5,3,2];
 
-const arr2=arr1.filter(function(value){
-    return value<5
-});
-console.log(arr1);
-console.log(arr2);
+const result =arr1.find(val)
+
+
+function val(element){
+    return element>2;
+}
+console.log(result);
