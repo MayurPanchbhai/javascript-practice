24dfd2dde65dfe8b1a3de9a2ee411ef2f7d7cf67
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-04-17 23:20:31 +0530

    all array methods complete

--- createCode.js
+++ createCode.js
@@ -1,8 +1,13 @@
-// toLocalstring(): - convert array elements to string
+// values:- this method creates a new array iterator object that carries the values specified at each array index. We can iterate the array elements via loops or iterator methods.
 
-const names=["Leclerc","Hamilton","Verstappen","Vettel"]
 
-let srt=names.toLocaleString();
+const arr=["v","a","l","u","e","s"];
+console.log(arr);
 
-console.log(srt);
-// console.log(typeof(srt));
+let val=arr.values();
+
+console.log(val);
+
+for (let x of val){
+    console.log(x);
+}
