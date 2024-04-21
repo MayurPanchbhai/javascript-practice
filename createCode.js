4e77552428dd75a530b851d1aa892b84ed71a3da
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-04-21 22:04:24 +0530

    define property()

--- createCode.js
+++ createCode.js
@@ -1,15 +1,16 @@
-// create() :-used to create new object from specified prototype of the object 
+// defineProperty(): used to define the that can't be changed 
 
-// Object.create(prototype[propertiesObject])
-
-const man ={
-    printIntro:function (){
-        console.log(`My name is ${this.name} . Am I machine? ${this.isMachine}`);
-    }
+// Object.defineProperty(objName, PropertyName, value)
+const obj={
+    name:"Mayur"
 }
 
-const me = Object.create(man);
-me.name="Gaurav";//name is property of the "me" but not on "person"
-me.isMachine ="May Be"//inherited property can be over written
+Object.defineProperty(obj , "ID" ,{
+    value:"panchbhaimayur",
+    writable:true
+}
+);
 
-me.printIntro();
+console.log(obj.ID)
+obj.ID="PanchbhaiGaurav";
+console.log(obj.ID);
