086cbb24152659750a9a42657132b1e26b08537f
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-04-22 14:21:19 +0530

    enteries() and freeze()

--- createCode.js
+++ createCode.js
@@ -1,16 +1,18 @@
-// defineProperty(): used to define the that can't be changed 
+// freeze(): freeze the object that prevent new prperty being add to it.
 
-// Object.defineProperty(objName, PropertyName, value)
-const obj={
-    name:"Mayur"
+// Object.freeze(object)
+
+const obj1={
+    name:"Maximus"
 }
 
-Object.defineProperty(obj , "ID" ,{
-    value:"panchbhaimayur",
-    writable:true
-}
-);
+const obj2 = Object.freeze(obj1);
 
-console.log(obj.ID)
-obj.ID="PanchbhaiGaurav";
-console.log(obj.ID);
+obj2.Fuel2="98octane";//new property can't be added here
+
+obj1.Fuel2="98octane";
+
+console.log(obj2);
+console.log(obj1);
+
+
