92127d1bd347c4a2c1506cd037ea9cad32520028
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-04-23 13:25:07 +0530

    getOwnPropertyNames() , getPrototypeOf() ,is()

--- createCode.js
+++ createCode.js
@@ -1,12 +1,15 @@
-// Object.getOwnPropertyDescriptors():_ This method returns all own property descriptors of a given object. The difference between getOwnPropertyDescriptors() and getOwnPropertyDescriptor() method is that getOwnPropertyDescriptors() method ignores symbolic properties.
+// Object.is():- method of JavaScript is used to determine whether two values are the same value. There is a special built-in method that compares values.
 
+// Object.is(value1, value2); 
 
-// Object.getOwnPropertyDescriptors(obj)  
+console.log(Object.is(5, 5)); // Output: true
+console.log(Object.is('hello', 'hello')); // Output: true
 
-const object1 = {  
-    property1: 103  
-};  
-    
-  const descriptors1 = Object.getOwnPropertyDescriptors(object1);  
-  console.log(descriptors1.property1.writable);  
-  console.log(descriptors1.property1.value);  
+console.log(Object.is(0, -0)); // Output: false
+console.log(Object.is(NaN, NaN)); // Output: true
+
+console.log(Object.is({}, {})); // Output: false (different object references)
+console.log(Object.is([], [])); // Output: false (different object references)
+
+const obj = {};
+console.log(Object.is(obj, obj)); // Output: true (same object reference)
