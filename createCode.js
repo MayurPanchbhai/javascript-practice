fbd262b751546df4f056e60834a1f9895a25a2c2
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-05-01 20:37:27 +0530

    String method

--- createCode.js
+++ createCode.js
@@ -1,11 +1,4 @@
-// Object.values() returns an array which contains the given object's own enumerable property values, in the same order as that provided by a for...in loop.
-
-// Object.values(obj)
+// String :- The string literal is created using double quotes. The syntax of creating string using string literal is given below:
 
 
-const object1 = {  
-    a: 'Rahul',  
-    b: 0,  
-    c:false  
-};  
-console.log(Object.values(object1)); 
+var stringname="string value"; 
