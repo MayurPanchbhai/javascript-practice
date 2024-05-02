7b49e52982c70bd22952d9a6ff787b1c8ce15fc3
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-05-02 12:50:31 +0530

    string methods :- charAt , charCodeAt , concat , indexOF , lastIndexOf , search

--- createCode.js
+++ createCode.js
@@ -1,4 +1,8 @@
-// String :- The string literal is created using double quotes. The syntax of creating string using string literal is given below:
+// search(): method is used to search the regular expression in the given string. This method returns -1, if match is not found.
 
+// String.search("ch/srt");
 
-var stringname="string value"; 
+let srt="A necessary sacrifice to bring peace to this planet. We can not let the humans pay for our mistakes.";
+
+console.log(srt.search("planet"));
+console.log(srt.search("Cybertron"));
